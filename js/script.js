4d36cffa6b1161aa31a8249bb846f91cc714ba24
SunYoung Song <ssy6616@naver.com>
2022-04-11 00:14:58 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// 명령 초기화 //
+// 번역 : 버튼을 누를때마다 div[i]가 위로 올라오게 된다. 
+
+
+
+const $div = document.querySelectorAll('div');
+const button = document.querySelector('button');
+
+console.log('button');
+console.log('$div');
+
+for(let i= 0;i<$div;i++){
+    $div[i]
+}
+
+
